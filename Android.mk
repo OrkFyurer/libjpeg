8539c4052d67cb5221acc13e00e10a57a9998d57
shunirr <shun262@gmail.com>
2012-05-29 17:08:47 +0900

    generate static library

--- Android.mk
+++ Android.mk
@@ -1,17 +1,8 @@
-# Makefile for libjpeg
- 
 LOCAL_PATH := $(my-dir)
- 
-######################################################
-###           libjpeg.so                       ##
-######################################################
  
 include $(CLEAR_VARS)
 
 LOCAL_SRC_FILES := jaricom.c jcapimin.c jcapistd.c jcarith.c jccoefct.c jccolor.c jcdctmgr.c jchuff.c jcinit.c jcmainct.c jcmarker.c jcmaster.c jcomapi.c jcparam.c jcprepct.c jcsample.c jctrans.c jdapimin.c jdapistd.c jdarith.c jdatadst.c jdatasrc.c jdcoefct.c jdcolor.c jddctmgr.c jdhuff.c jdinput.c jdmainct.c jdmarker.c jdmaster.c jdmerge.c jdpostct.c jdsample.c jdtrans.c jerror.c jfdctflt.c jfdctfst.c jfdctint.c jidctflt.c jidctfst.c jidctint.c jquant1.c jquant2.c jutils.c jmemmgr.c jmemname.c
- 
-LOCAL_SHARED_LIBRARIES := libcutils
-LOCAL_STATIC_LIBRARIES := libsimd
  
 LOCAL_C_INCLUDES := $(LOCAL_PATH) 
  
@@ -20,8 +11,7 @@ LOCAL_CFLAGS := -DAVOID_TABLES  -O3 -fstrict-aliasing -fprefetch-loop-arrays  -D
 
 LOCAL_MODULE_PATH := $(TARGET_OUT_OPTIONAL_STATIC_LIBRARY)
  
-LOCAL_MODULE_TAGS := debug
- 
 LOCAL_MODULE := libjpeg
 
-include $(BUILD_SHARED_LIBRARY)
+include $(BUILD_STATIC_LIBRARY)
+
